d27590100d53efd6c5f8808bf53f81423ffaa5f0
Hitarth Desai <83507760+hitarthdesai@users.noreply.github.com>
2023-12-02 18:01:38 -0500

    day 2 part 2

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,20 +1,20 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("./src/input.txt").unwrap();
-    let p1: i32 = input.lines().map(p1_calibration_value).sum();
-    let p2: i32 = input.lines().map(p2_calibration_value).sum();
+    let input = fs::read_to_string("input.txt").unwrap();
+    let p1: i32 = input.lines().map(part1).sum();
+    let p2: i32 = input.lines().map(part2).sum();
     println!("Part1: {p1}");
     println!("Part2: {p2}");
 }
 
-fn p1_calibration_value(line: &str) -> i32 {
+fn part1(line: &str) -> i32 {
     let d1 = line.chars().find(|c| c.is_numeric()).unwrap_or('0').to_digit(10).unwrap_or(0) as i32;
     let d2 = line.chars().rfind(|c| c.is_numeric()).unwrap_or('0').to_digit(10).unwrap_or(0) as i32;
     return d1 * 10 + d2
 }
 
-fn p2_calibration_value(line: &str) -> i32 {
+fn part2(line: &str) -> i32 {
     let line = line
         .replace("one", "o1e")
         .replace("two", "t2o")
@@ -25,5 +25,5 @@ fn p2_calibration_value(line: &str) -> i32 {
         .replace("seven", "s7n")
         .replace("eight", "e8t")
         .replace("nine", "n9e");
-    p1_calibration_value(&line)
+    part1(&line)
 }
--- src/day2/main.rs
+++ src/day2/main.rs
@@ -1,12 +1,13 @@
 use std::fs;
+use std::cmp::max;
 const MAX: [i32; 3] = [12, 13, 14];
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let p1: i32 = input.lines().map(part1).sum();
+    let p2: i32 = input.lines().map(part2).sum();
     println!("Part1: {p1}");
-    // let p2: i32 = input.lines().map(p2_calibration_value).sum();
-    // println!("Part2: {p2}");
+    println!("Part2: {p2}");
 }
 
 fn part1(line: &str) -> i32 {
@@ -35,4 +36,29 @@ fn part1(line: &str) -> i32 {
     }
 
     return game_id
+}
+
+fn part2(line: &str) -> i32 {
+    let parts = line.split(':');
+    let game_id: i32 = parts.clone().nth(0).unwrap().split(' ').nth(1).unwrap().parse().unwrap();
+    let sets = parts.clone().nth(1).unwrap().split("; ").map(|s| s.trim().split(", "));
+
+    let mut counts = [0, 0, 0];
+
+    for set in sets {
+        for s in set {
+            let mut colors = s.split(" ");
+            let f: i32 = colors.next().unwrap().parse().unwrap();
+            let color = colors.next().unwrap();
+
+            match color {
+                "red" => counts[0] = max(counts[0], f),
+                "green" => counts[1] = max(counts[1], f),
+                "blue" => counts[2] = max(counts[2], f),
+                _ => {},
+            }
+        }
+    }
+
+    return counts[0] * counts[1] * counts[2]
 }
